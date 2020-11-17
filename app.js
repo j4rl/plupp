d5f2cedc101e9a1332e1a059b9dc300416a0c54c
Charlie Jarl <charlie.jarl@gmail.com>
2020-11-17 13:27:29 +0100

    smol chingis

--- app.js
+++ app.js
@@ -4,10 +4,28 @@ document.addEventListener("DOMContentLoaded", function(){
     const mainWidth=600;
     const mainHeight=600;
     const pluppSize=50;
+    let isGoingDown=false;
+    let isGoingUp=false;
+    let isGoingLeft=false;
+    let isGoingRight=false;
+    let pluppLeft;
+    let pluppTop;
 
     function init (){
-        plupp.style.top=((mainHeight/2)-(pluppSize/2))+'px';
-        plupp.style.left=((mainWidth/2)-(pluppSize/2))+'px';
+        pluppTop=((mainHeight/2)-(pluppSize/2));
+        pluppLeft=((mainWidth/2)-(pluppSize/2));
+        plupp.style.top=pluppTop+'px';
+        plupp.style.left=pluppLeft+'px';
+        box.style.width=mainWidth+'px';
+        box.style.height=mainHeight+'px';
+        isGoingLeft=false;
+        isGoingRight=false;
+        isGoingUp=false;
+        isGoingDown=false;
+    }
+
+    function moveLeft(){
+        //if inte på vänsterkant flytta vänster else flytta höger
     }
 
     function main(){
