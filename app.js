a74f5c77849d7c86cbb2a33fe06b9367b28410d4
Charlie Jarl <charlie.jarl@gmail.com>
2021-08-17 15:19:24 +0200

    Bob is way.

--- app.js
+++ app.js
@@ -1,35 +1,142 @@
-document.addEventListener("DOMContentLoaded", function(){
-    const box = document.querySelector(".main");
-    const plupp=document.querySelector(".plupp");
-    const mainWidth=600;
-    const mainHeight=600;
-    const pluppSize=50;
-    let isGoingDown=false;
-    let isGoingUp=false;
-    let isGoingLeft=false;
-    let isGoingRight=false;
-    let pluppLeft;
-    let pluppTop;
-
-    function init (){
-        pluppTop=((mainHeight/2)-(pluppSize/2));
-        pluppLeft=((mainWidth/2)-(pluppSize/2));
-        plupp.style.top=pluppTop+'px';
-        plupp.style.left=pluppLeft+'px';
-        box.style.width=mainWidth+'px';
-        box.style.height=mainHeight+'px';
-        isGoingLeft=false;
-        isGoingRight=false;
-        isGoingUp=false;
-        isGoingDown=false;
+document.addEventListener("DOMContentLoaded", function () {
+    const box = document.querySelector(".box");
+    const bob = document.querySelector(".bob");
+    const disp = document.querySelector(".disp");
+    const boxSize = 500;
+    let bobSize = 50;
+    let bobSpeed = boxSize / 100 + 2;
+    let hits = 0;
+    let bobLeft = boxSize / 2 - bobSize / 2;
+    let bobTop = boxSize / 2 - bobSize / 2;
+    let maxBob = boxSize / 2;
+    let isGoingUp = false;
+    let isGoingDown = false;
+    let isGoingRight = false;
+    let isGoingLeft = false;
+    let upTimer;
+    let downTimer;
+    let rightTimer;
+    let leftTimer;
+  
+    function init() {
+      bob.style.left = bobLeft + "px";
+      bob.style.top = bobTop + "px";
+      box.style.width = boxSize + "px";
+      box.style.height = boxSize + "px";
+      disp.style.width = boxSize + "px";
+      coords();
     }
-
-    function moveLeft(){
-        //if inte på vänsterkant flytta vänster else flytta höger
+    function stopHammerTime() {
+      isGoingUp = false;
+      isGoingDown = false;
+      isGoingRight = false;
+      isGoingLeft = false;
     }
-
-    function main(){
-        init();
+    function timerReset() {
+      clearInterval(upTimer);
+      clearInterval(downTimer);
+      clearInterval(rightTimer);
+      clearInterval(leftTimer);
+    }
+  
+    function moveUp() {
+      stopHammerTime();
+      isGoingUp = true;
+  
+      //timerReset();
+      // upTimer = setInterval(function () {
+      if (bobTop >= 0) {
+        bobTop -= bobSpeed;
+        bob.style.top = bobTop + "px";
+      } else {
+        moveDown();
+        grow();
+      }
+      //   });
+    }
+    function moveDown() {
+      stopHammerTime();
+      isGoingDown = true;
+      //timerReset();
+      //   downTimer = setInterval(function () {
+      if (bobTop + 10 + bobSize <= boxSize) {
+        bobTop += bobSpeed;
+        bob.style.top = bobTop + "px";
+      } else {
+        moveUp();
+        grow();
+      }
+      //  });
+    }
+    function moveRight() {
+      stopHammerTime();
+      isGoingRight = true;
+      //timerReset();
+      //  rightTimer = setInterval(function () {
+      if (bobLeft <= boxSize - (bobSize + 10)) {
+        bobLeft += bobSpeed;
+        bob.style.left = bobLeft + "px";
+      } else {
+        moveLeft();
+        grow();
+      }
+      //   });
+    }
+    function moveLeft() {
+      stopHammerTime();
+      isGoingLeft = true;
+      //timerReset();
+      //leftTimer = setInterval(function () {
+      if (bobLeft >= 0) {
+        bobLeft -= bobSpeed;
+        bob.style.left = bobLeft + "px";
+      } else {
+        moveRight();
+        grow();
+      }
+      // });
+    }
+    function grow() {
+      hits++;
+      bob.innerHTML = hits;
+      bobSize += bobSpeed;
+      //if (bobSize <= maxBob) {
+      bob.style.width = bobSize + "px";
+      bob.style.height = bobSize + "px";
+      //  } else {
+      //gameover?
+      //    }
+    }
+    function control(e) {
+      switch (e.key) {
+        case "ArrowUp":
+          moveUp();
+          break;
+        case "ArrowDown":
+          moveDown();
+          break;
+        case "ArrowRight":
+          moveRight();
+          break;
+        case "ArrowLeft":
+          moveLeft();
+          break;
+        default:
+          break;
+      }
+      coords();
+    }
+  
+    function coords() {
+      let Y = bobTop + bobSize / 2;
+      let X = bobLeft + bobSize / 2;
+      disp.innerHTML = X + ", " + Y;
+    }
+  
+    function main() {
+      init();
+      document.addEventListener("keydown", control);
     }
     main();
-});
+  });
+  
